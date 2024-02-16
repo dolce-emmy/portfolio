5f6cff65d2e119cf277a66bca68b655773b4dd72
Eman Yassin <eman.yassin83@gmail.com>
2024-02-16 11:14:44 +0100

    added some changes

--- portfolio/src/app/components/Confetti.js
+++ portfolio/src/app/components/Confetti.js
@@ -1,45 +1,45 @@
-import React from 'react'
+// import React from 'react'
 
-const Confetti = () => {
-  //   const [showConfetti, setShowConfetti] = useState(false);
+// const Confetti = () => {
+//   const [showConfetti, setShowConfetti] = useState(false);
 
-  //   const [brightness, setBrightness] = useState(false);
-  //   useEffect(() => {
+//   const [brightness, setBrightness] = useState(false);
+//   useEffect(() => {
 
-  //     setShowConfetti(true);
-  //     const timeoutId = setTimeout(() => {
-  //       setShowConfetti(false);
-  //     }, 4000);
+//     setShowConfetti(true);
+//     const timeoutId = setTimeout(() => {
+//       setShowConfetti(false);
+//     }, 4000);
 
-  // const handleScroll = () => {
-    
-  // here I am saying if brightness is false, then set it to true
-  //   if (!brightness) {
-  //     setBrightness(true);
-  //   }
-  // };
+// const handleScroll = () => {
 
-  // window.addEventListener("scroll", handleScroll);
+// here I am saying if brightness is false, then set it to true
+//   if (!brightness) {
+//     setBrightness(true);
+//   }
+// };
 
-  // return () => {
-  //   clearTimeout(timeoutId);
-  //   window.removeEventListener("scroll", handleScroll);
+// window.addEventListener("scroll", handleScroll);
 
-  // };
+// return () => {
+//   clearTimeout(timeoutId);
+//   window.removeEventListener("scroll", handleScroll);
 
-  //   }, [brightness]);
+// };
 
-  return (
-    <div>
-      {/* <Confetti
+//   }, [brightness]);
+
+// return (
+//   <div>
+/*{ <Confetti
         width={1920}
         height={1030}
         numberOfPieces={showConfetti ? 200 : 0}
         // here the initial velocity controls the direction of the falling confetti and it's adjusted to 50 to make it fall in a straight line
         initialVelocityY={50}
-      /> */}
-    </div>
-  );
-}
+      /> }*/
+//</div>
+//   );
+// }
 
-export default Confetti
+// export default Confetti
